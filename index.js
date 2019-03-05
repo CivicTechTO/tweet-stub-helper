d7c710410391b44cc7bd7fd7c1e69072af102cd5
Patrick Connolly <patrick.c.connolly@gmail.com>
2019-03-05 17:49:42 -0500

    Allowing redirect and button page to happen from one page.

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 // Scaffolded from: https://router.vuejs.org/guide/#javascript
 
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
+}
+
 // Redirect before entering the route.
 const routes = [
   { path: '/',
     beforeEnter(to, from, next) {
+      if (!["1", "true", "yes"].includes(getQueryVariable('redirect'))) {
+        return
+      }
       const key = '1-p0CyUMC0nqrEQNc6Yikd2vg033GoChSWR8rFKFxfgU'
       const id = '1209202081'
       const csvUrl = `https://docs.google.com/spreadsheets/d/${key}/export?format=csv&id=${key}&gid=${id}`
